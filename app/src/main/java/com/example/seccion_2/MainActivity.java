8f38753c5700c4451e167f7b7bb804781ee9b4d8
jreyes1990 <jarl1990mt@gmail.com>
2023-04-09 18:19:25 -0600

    Uso de Simple ListView - Click.

--- app/src/main/java/com/example/seccion_2/MainActivity.java
+++ app/src/main/java/com/example/seccion_2/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.seccion_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
   private ListView listView;
+  private List<String> names;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     listView = (ListView) findViewById(R.id.listView);
 
     // Datos a mostrar
-    List<String> names = new ArrayList<String>();
+    names = new ArrayList<String>();
     names.add("Alejandro");
     names.add("Fernando");
     names.add("Ruben");
@@ -31,5 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
     // Enlazamos el adaptador con nuestro List View
     listView.setAdapter(adapter);
+
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(MainActivity.this, "Clicked: "+ names.get(position), Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 }
